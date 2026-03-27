9a61ecd0f0de046eac745b72f18ba41d8a8d2637
krookskala <ismailsariarslan7@gmail.com>
2026-03-27 14:56:06 +0100

    Add Header component in Angular frontend with logout functionality and Material design integration for enhanced user navigation.

---
+++ frontend/src/app/layout/header/header.ts
@@ -0,0 +1,20 @@
+import { Component, inject } from "@angular/core";
+import { Router } from "@angular/router";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatButtonModule } from "@angular/material/button";
+
+@Component({
+    selector: 'app-header',
+    standalone: true,
+    imports: [MatToolbarModule, MatButtonModule],
+    templateUrl: './header.html',
+    styleUrl: './header.scss'
+})
+
+export class Header {
+    private router = inject(Router);
+    logout() {
+        localStorage.removeItem('token');
+        this.router.navigate(['/login']);
+    }
+}
