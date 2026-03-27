d9aadb3510b4ce8b48e76f1712183e8860c5f48c
krookskala <ismailsariarslan7@gmail.com>
2026-03-27 14:57:13 +0100

    Refactor routing structure in Angular frontend to include Layout component and nested routes for Dashboard, Employees, and Departments, enhancing application organization and navigation.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,22 @@
 import { Routes } from '@angular/router';
 import { Login } from './features/auth/login';
+import { Layout } from './layout/layout';
 import { Dashboard } from './features/dashboard/dashboard';
 import { authGuard } from './core/guards/auth.guard';
+import { EmployeeList } from './features/employees/employee-list';
+import { DepartmentList } from './features/departments/department-list';
 
 export const routes: Routes = [
     { path: 'login', component: Login },
     { path: '', redirectTo: 'login', pathMatch: 'full'},
-    { path: 'dashboard', component: Dashboard , canActivate: [authGuard] }
+    {
+        path: '',
+        component: Layout,
+        canActivate: [authGuard],
+        children: [
+            { path: 'dashboard', component: Dashboard },
+            { path: 'employees', component: EmployeeList},
+            { path: 'departments', component: DepartmentList}
+        ]
+    }
 ];
