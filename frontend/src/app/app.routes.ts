72b8bca061ef6669ade3387f557adb4b95053ca9
krookskala <ismailsariarslan7@gmail.com>
2026-03-27 13:47:17 +0100

    Add routing for login and dashboard components in Angular frontend, integrating auth guard for route protection and redirecting to login by default.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Login } from './features/auth/login';
+import { Dashboard } from './features/dashboard/dashboard';
+import { authGuard } from './core/guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'login', component: Login },
+    { path: '', redirectTo: 'login', pathMatch: 'full'},
+    { path: 'dashboard', component: Dashboard , canActivate: [authGuard] }
+];
