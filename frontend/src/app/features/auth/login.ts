153e90c409954ff39c32be9996b44a69163ca153
krookskala <ismailsariarslan7@gmail.com>
2026-03-27 13:47:12 +0100

    Enhance login component in Angular frontend by integrating Angular Material modules for improved UI and adding navigation to the dashboard upon successful login.

--- frontend/src/app/features/auth/login.ts
+++ frontend/src/app/features/auth/login.ts
@@ -1,12 +1,16 @@
 import { Component } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { AuthService } from "../../core/services/auth.service";
-
+import { MatCardModule } from "@angular/material/card";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from "@angular/material/button";
+import { Router } from "@angular/router";
 
 @Component({
     selector: 'app-login',
     standalone: true,
-    imports: [FormsModule],
+    imports: [FormsModule, MatCardModule, MatFormFieldModule, MatInputModule, MatButtonModule],
     templateUrl: './login.html',
     styleUrl: './login.scss'
 })
@@ -16,13 +20,14 @@ export class Login {
     password = '';
     error = '';
 
-    constructor(private authService: AuthService) {}
+    constructor(private authService: AuthService, private router: Router) {}
 
     login() {
         this.authService.login({ email: this.email, password: this.password
          }).subscribe({
             next: (response) => {
                 localStorage.setItem('token', response.token);
+                this.router.navigate(['/dashboard']);
             },
             error: (err) => {
                 this.error = 'Email or Password Wrong';
