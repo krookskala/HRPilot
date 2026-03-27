cf046cdcbbfaa773ca5a3d88e1287e1961ae578b
krookskala <ismailsariarslan7@gmail.com>
2026-03-27 15:38:21 +0100

    Add createEmployee method to EmployeeService in Angular frontend, enabling the creation of new employee entries through API integration.

--- frontend/src/app/core/services/employee.service.ts
+++ frontend/src/app/core/services/employee.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { environment } from "../../../environments/environment";
 import { Observable } from "rxjs";
-import { Employee } from "../../shared/models/employee.model";
+import { CreateEmployeeRequest, Employee } from "../../shared/models/employee.model";
 
 @Injectable({ providedIn: 'root' })
 export class EmployeeService {
@@ -15,5 +15,9 @@ export class EmployeeService {
 
     deleteEmployee(id: number): Observable<void> {
         return this.http.delete<void>(`${this.apiUrl}/employees/${id}`);
+    }
+
+    createEmployee(request: CreateEmployeeRequest): Observable<Employee> {
+        return this.http.post<Employee>(`${this.apiUrl}/employees`, request);
     }
 }
