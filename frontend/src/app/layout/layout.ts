a33697cdcbd663ae16458de9121d31f6ce7675e9
krookskala <ismailsariarslan7@gmail.com>
2026-03-27 14:56:11 +0100

    Add Layout component in Angular frontend to structure application with Header and Sidebar integration for improved navigation.

---
+++ frontend/src/app/layout/layout.ts
@@ -0,0 +1,16 @@
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { Header } from "./header/header";
+import { Sidebar } from "./sidebar/sidebar";
+
+@Component({
+    selector: 'app-layout',
+    standalone: true,
+    imports: [RouterOutlet, Header, Sidebar],
+    templateUrl: './layout.html',
+    styleUrl: './layout.scss'
+})
+
+export class Layout {
+    
+}
