cbf6b02f8e2e53d38cbab86ea816dea9f037ea98
krookskala <ismailsariarslan7@gmail.com>
2026-03-29 13:40:28 +0200

    Enhance header component by adding MatIcon and MatTooltip modules to improve visual elements and user interaction.

--- frontend/src/app/layout/header/header.ts
+++ frontend/src/app/layout/header/header.ts
@@ -2,11 +2,13 @@ import { Component, inject } from "@angular/core";
 import { Router } from "@angular/router";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { MatTooltipModule } from "@angular/material/tooltip";
 
 @Component({
     selector: 'app-header',
     standalone: true,
-    imports: [MatToolbarModule, MatButtonModule],
+    imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatTooltipModule],
     templateUrl: './header.html',
     styleUrl: './header.scss'
 })
