ac86ff7e641aada22437aa1eb69f26395ffd5463
krookskala <ismailsariarslan7@gmail.com>
2026-03-30 14:50:34 +0200

    Add StorageConfiguration class to configure storage properties. This new class enables the application to utilize StorageProperties for managing file storage configurations, enhancing flexibility in handling storage options.

---
+++ backend/src/main/java/com/hrpilot/backend/common/storage/StorageConfiguration.java
@@ -0,0 +1,9 @@
+package com.hrpilot.backend.common.storage;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(StorageProperties.class)
+public class StorageConfiguration {
+}
